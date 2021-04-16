df49f9ce4de7457fee31f930769c756751203c7c
Jesse Ball <jball@cbhs-sacramento.org>
2021-04-16 13:24:19 -0700

    cleaned if/else

--- Delete-TeamsCache.ps1
+++ Delete-TeamsCache.ps1
@@ -5,15 +5,13 @@ This script allows you to cleanup the Microsoft Teams app and remove the cache f
 Modified from https://techcommunity.microsoft.com/t5/itops-talk-blog/powershell-basics-how-to-delete-microsoft-teams-cache-for-all/ba-p/1519118 
 #>
 If ($IsMacOS) {
-    Get-ChildItem "~/Library/Application Support/Microsoft/Teams/*" -directory | `
-    Where-Object name -in ('application cache','blob storage','databases','GPUcache','IndexedDB','Local Storage','tmp') | `
-    ForEach-Object { 
-        Remove-Item $_.FullName -Recurse -Force 
-    }
-} Else {
-    Get-ChildItem "C:\Users\*\AppData\Roaming\Microsoft\Teams\*" -directory | `
-    Where-Object name -in ('application cache','blob storage','databases','GPUcache','IndexedDB','Local Storage','tmp') | `
-    ForEach-Object {
-        Remove-Item $_.FullName -Recurse -Force
-    }
+    $folder_path = "~/Library/Application Support/Microsoft/Teams/*"
+} elseif ($IsWindows) {
+    $folder_path = "C:\Users\*\AppData\Roaming\Microsoft\Teams\*"
+}
+
+Get-ChildItem $folder_path -directory | `
+Where-Object name -in ('application cache','blob storage','databases','GPUcache','IndexedDB','Local Storage','tmp') | `
+ForEach-Object { 
+    Remove-Item $_.FullName -Recurse -Force
 }
