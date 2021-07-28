2e7a2fd28785cdbf35dfe612cc425ac62844bd02
Jesse Ball <jesseb.all@icloud.com>
2021-07-28 16:00:31 -0700

    Working commit

--- Update-LocalADUserAccts.ps1
+++ Update-LocalADUserAccts.ps1
@@ -2,6 +2,7 @@
 .SYNOPSIS
 
 This script is used to add the organization address to specific users.
+
 #>
 Import-Module ActiveDirectory
 
@@ -14,10 +15,13 @@ function RecurseGroups {
     
     $getGroup = Get-ADGroup -Filter "GroupCategory -eq 'Security' -and Name -eq '$($groupName)'"
     $getGroupMembers = $getGroup | Get-ADGroupMember -Recursive `
-    | Select-Object SamAccountName -Unique -First 1
+    | Select-Object SamAccountName -Unique
 
     Foreach ($member in $getGroupMembers) {
-        Get-ADUser $member.SamAccountName -Properties *
+        Get-ADUser $member.SamAccountName -Properties City,Company,HomePage,PostalCode,st,State,StreetAddress `
+        | Set-ADUser -City "Sacramento" -Company "Christian Brothers High School" -HomePage "https://www.cbhs-sacramento.org"`
+        -PostalCode "95820" -State "CA" -StreetAddress "4315 Martin Luther King, Jr. Blvd." #-WhatIf
+        # https://docs.microsoft.com/en-us/powershell/module/activedirectory/set-aduser?view=winserver2012r2-ps&redirectedfrom=MSDN
     }
     
 }
